e462e3643c7212ac80d590f7a8fa2a2a0ac395a8
Sigmales <yantoubri@gmail.com>
2025-08-24 07:57:30 +0000

    Added the 2nd line

--- program1.py
+++ program1.py
@@ -1,2 +1,8 @@
 #----------------------------------------
 print("Added this file for collaboration from Local Machine")
+#-----------------------------------
+print("Added a 2nd line")
+#---------------------------------
+print("Added a 3rd line")
+
+
---
+++ program2.py
@@ -0,0 +1,4 @@
+#----------------------------------------
+print("Added this file for collaboration from Local Machine")
+#-----------------------------------
+print("Added a 2nd line")
